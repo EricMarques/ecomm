c7d0bc920110629b5cd4c7d6122beaf43cba75bc
Eric Marques <erc_m@hotmail.com>
2020-07-13 23:19:14 -0300

    ajustes

--- ecomm/settings_prod.py
+++ ecomm/settings_prod.py
@@ -7,13 +7,7 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
-STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
