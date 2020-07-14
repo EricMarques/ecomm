024e3e8800e91c9078c44b0c1715a33308519810
Eric Marques <erc_m@hotmail.com>
2020-07-13 23:21:25 -0300

    ajustes

--- ecomm/wsgi.py
+++ ecomm/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomm.settings_prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomm.settings')
 
 application = get_wsgi_application()
