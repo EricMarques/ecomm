754e137c4cf19b5cdcb3046853b9af1ea28e90bc
Eric Marques <erc_m@hotmail.com>
2020-07-13 23:00:02 -0300

    ajustes

--- ecomm/settings_prod.py
+++ ecomm/settings_prod.py
@@ -9,10 +9,6 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
